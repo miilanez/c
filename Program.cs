4492b7d732a85431d5274c1a8f61e2971fb2062f
miilanez <33701681+miilanez@users.noreply.github.com>
2019-10-18 01:33:46 -0300

    Add files via upload
    Atividade de simulação de um Caixa Eletrônico

--- Program.cs
+++ Program.cs
@@ -4,34 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Atividade01
+namespace CaixaEletronico
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Digite uma frase: ");
-            string frase = Convert.ToString(Console.ReadLine());
-
-            int TamanhoFrase = frase.Length;
-
-            string letraA = frase.Substring(0, 5);
-            string letraB = frase.Substring(TamanhoFrase - 5, 5);
-
-            string fraseInversa = "";
-            int posicao = TamanhoFrase - 1;
-
-            for (; posicao <= 0; posicao++)
             {
-                fraseInversa = fraseInversa + frase.Substring(posicao, 1);
+                Console.WriteLine("QUANTIDADE QUE DESEJA RETIRAR: ");
+                int VALOR = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(VALOR % 1000);
+                Console.WriteLine("Quantidades de notas de 100: " + VALOR / 100);
+                VALOR = VALOR - (VALOR / 100 * 100);
+                Console.WriteLine("Quantidades de notas de 50: " + (VALOR / 50));
+                VALOR = VALOR - (VALOR / 50 * 50);
+                Console.WriteLine("Quantidades de notas de 20: " + (VALOR / 20));
+                VALOR = VALOR - ((VALOR / 20) * 20);
+                Console.WriteLine("Quantidades de notas de 10: " + (VALOR / 10));
+                VALOR = VALOR - ((VALOR / 10) * 10);
+                Console.WriteLine("Quantidades de notas de 5: " + (VALOR / 5));
+                VALOR = VALOR - ((VALOR / 5) * 5);
+                Console.WriteLine("Quantidades de notas de 2: " + (VALOR / 2));
+                VALOR = VALOR - ((VALOR / 2) * 2);
+                Console.WriteLine("Quantidades notas de 1: " + (VALOR));
+
+                Console.ReadLine();
+
             }
-
-            Console.WriteLine("5 primeiras letras: " + letraA);
-            Console.WriteLine("5 ultimas letras: " + letraB);
-            Console.WriteLine("5 primeiras letras inversas: " + fraseInversa);
-            Console.WriteLine("Tamanho da frase: " + TamanhoFrase);
-
-            Console.Read();
         }
     }
 }
